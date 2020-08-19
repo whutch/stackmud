d9ac8421ca49df67d0c3e877b6ec334216bb0d98
Will Hutcheson <will.hutcheson@gmail.com>
2020-08-18 20:49:40 -0500

    Add a barebones test to make sure CI works

---
+++ tests/test_meta.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Tests for the whole MUD process via a dummy client."""
+# Part of StackMUD (https://github.com/whutch/stackmud)
+# :copyright: (c) 2020 Will Hutcheson
+# :license: MIT (https://github.com/whutch/stackmud/blob/master/LICENSE.txt)
+
+from telnetlib import Telnet
+
+from stackmud import get_version
+
+
+class TestMeta:
+
+    """A collection of tests for the whole MUD process."""
+
+    client = Telnet()
+
+    def test_meta(self):
+        """Test several client interactions."""
