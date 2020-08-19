ccc09441659e7c3ec669d9f953423e7c7e05b3ac
Will Hutcheson <will.hutcheson@gmail.com>
2020-08-18 19:01:06 -0500

    Add CI and test/coverage configuration

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Configuration for testing through py.test."""
+# Part of StackMUD (https://github.com/whutch/stackmud)
+# :copyright: (c) 2020 Will Hutcheson
+# :license: MIT (https://github.com/whutch/stackmud/blob/master/LICENSE.txt)
